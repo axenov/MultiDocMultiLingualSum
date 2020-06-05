c1b80bc34145002fc5f8bc7f404e4636c91d07a7
remi calizzano <remi.calizzano@gmail.com>
2020-06-05 13:51:42 +0200

    Debug

--- train/run_training.py
+++ train/run_training.py
@@ -6,8 +6,8 @@ from scripts.bert2bert import Bert2BertSummarizationTrainer
 import wandb
 
 wandb.login()
-Bert2BertSummarizationTrainer.train("train/args/bert2bert.json")
-T5SummarizationTrainer.train("train/args/t5.json")
+#Bert2BertSummarizationTrainer.train("train/args/bert2bert.json")
+#T5SummarizationTrainer.train("train/args/t5.json")
 BartSummarizationTrainer.train("train/args/bart.json")
 T5WithTitleSummarizationTrainer.train("train/args/t5_with_title.json")
 BartSummarizationTrainer.train("train/args/bart_cnn.json")
